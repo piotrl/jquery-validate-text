f7ba66e27d1f8b5d4072ff8d081bbc5f8bd8dbcc
Piotr Lewandowski <grovman@gmail.com>
2016-04-21 19:52:04 +0200

    Fix multiple inputs validation + Refactor

--- src/jquery.validator.js
+++ src/jquery.validator.js
@@ -1,51 +1,60 @@
 (function ($) {
     'use strict';
-    var settings = {
+    var defaults = {
         ERROR_CLASS: 'has-error',
         EMAIL_REGEX: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
         options: {}
     };
 
     $.fn.validateText = function (options) {
-        settings.options = this.extend({}, settings.options, options);
+        var settings = this.extend({}, defaults);
+        settings.options = this.extend({}, defaults.options, options);
 
         return this.filter('input').each(function () {
             var $el = $(this);
 
             $el.change(validate);
         });
+
+        function validate() {
+            var $input = $(this);
+            var text = $input.val();
+            var isValid = true;
+
+            if (isValid && settings.options.pattern) {
+                isValid = validatePattern(text);
+            }
+            if (isValid && settings.options.email) {
+                isValid = validateEmail(text);
+            }
+            errorHanding($input, isValid);
+        }
+
+        function errorHanding($input, isValid) {
+            $input.parent().toggleClass(
+                settings.ERROR_CLASS, !isValid
+            );
+
+            $input.closest("form")
+                .find('button[type="submit"]')
+                .prop("disabled", !isValid);
+        }
+
+        function validateEmail(text) {
+            return validatePattern(text, settings.EMAIL_REGEX);
+        }
+
+        function validatePattern(text, pattern) {
+            pattern = pattern || settings.options.pattern;
+            return pattern.test(text);
+        }
     };
-
-
-    function validate() {
-        var $el = $(this);
-        var text = $el.val();
-
-        if (settings.options.pattern) {
-            validatePattern(text, $el);
-        }
-        if (settings.options.email) {
-            validateEmail(text, $el);
-        }
-    }
-
-    function validateEmail(text, $el) {
-        validatePattern(text, $el, settings.EMAIL_REGEX);
-    }
-
-    function validatePattern(text, $el, pattern) {
-        pattern = pattern || settings.options.pattern;
-
-        $el.parent().toggleClass(
-            settings.ERROR_CLASS, !pattern.test(text)
-        );
-    }
 }(jQuery));
 
-$('#email').validateText({
+$('#name').validateText({
     pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłóńśźż]*$/
 });
 
-$('#name').validateText({
+$('#email').validateText({
     email: true
 });
